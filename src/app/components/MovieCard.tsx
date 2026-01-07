1c4393e2112c334cf9dfb5408018630e5c30b2cb
enhhtogtoh <enhhtogtoh@gmail.com>
2026-01-07 12:49:36 +0800

    ssss

--- src/app/components/MovieCard.tsx
+++ src/app/components/MovieCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 type Movie = {
   id: number;
   title: string;
@@ -8,31 +9,34 @@ type Movie = {
 type Props = {
   movie: Movie;
 };
+
 export const MovieCard = ({ movie }: Props) => {
   return (
-    <div
-      className="cursor-pointer bg-[#F4F4F5] h-97.5 rounded-md flex flex-col transition-transform duration-200 
+    <Link href={`/movie/${movie.id}`}>
+      <div
+        className="cursor-pointer bg-[#F4F4F5] h-97.5 rounded-md flex flex-col transition-transform duration-200 
              hover:scale-105 hover:drop-shadow-md"
-    >
-      <div className="h-75 w-full ">
-        <Image
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt={movie.title}
-          width={200}
-          height={300}
-          className="h-75 w-full rounded-lg object-cover "
-        />
-      </div>
-      <div className="flex flex-col pl-2">
-        <p className="mt-2 text-sm text-yellow-500">
-          ⭐ {movie.vote_average?.toFixed(1)}
-        </p>
+      >
+        <div className="h-75 w-full ">
+          <Image
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+            width={200}
+            height={300}
+            className="h-75 w-full rounded-lg object-cover "
+          />
+        </div>
+        <div className="flex flex-col pl-2">
+          <p className="mt-2 text-sm text-yellow-500">
+            ⭐ {movie.vote_average?.toFixed(1)}
+          </p>
 
-        <p className="text-lg md:text-base sm:text-sm font-medium text-black ">
-          {movie.title}
-        </p>
+          <p className="text-lg md:text-base sm:text-sm font-medium text-black ">
+            {movie.title}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 type nowPlaying = {
